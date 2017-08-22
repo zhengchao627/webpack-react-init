4ea4c13ddf72013055772619f0c83fb98226442b
zhengchao727 <13522612182@163.com>
2017-08-22 15:35:04 +0800

    webpack初始化 react 项目

---
+++ app/components/Component1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Component1 extends React.Component {
+    render() {
+        return (
+            <div>Hello W自己学习webpack orld!</div>
+        )
+    }
+}
+
+//导出组件
+export default Component1;
